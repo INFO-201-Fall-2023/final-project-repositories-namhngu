197446818c042c380c6151520284fa2d72e508e9
Nam Nguyen <113055309+namhngu@users.noreply.github.com>
2023-05-03 01:56:47 -0700

    added file to help gen columns for quarter and year

--- Gen_Quarter_Columns.R
+++ Gen_Quarter_Columns.R
@@ -19,5 +19,10 @@ vaccines_df <- read.csv("Washington_County_Vaccinations.csv")
 year_vaccines <- c()
 quarter_vaccines <- c()
 for (i in 1:nrow(vaccines_df)) {
-  
+  date <- strsplit(vaccines_df$Date[i], "/")[[1]]
+  year_vaccines <- append(year_vaccines, date[3])
+  quarter_vaccines <- append(quarter_vaccines,ceiling(strtoi(date[1])/3))
 }
+
+vaccines_df$Year <- year_vaccines
+vaccines_df$Quarter <- quarter_vaccines
