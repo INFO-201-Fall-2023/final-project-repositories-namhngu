eecd970a7e39da5545ab68137f24090d231ee884
Nam Nguyen <113055309+namhngu@users.noreply.github.com>
2023-05-07 04:31:28 -0700

    Finished 1st half of Data Wrangling assignment. Added new columns and summarization code along with our unification code.
    Finished 1st half of Data Wrangling assignment. Added new columns and summarization code along with our unification code.

--- JoinDataSets.R
+++ JoinDataSets.R
@@ -69,8 +69,14 @@ v_df <- cleanVaccinesDf(v_df)
 
 r_df <- attachCountiesToRev(r_df)
 
+#Convert Total.Taxable to numeric instead of string
+for (i in 1:nrow(r_df)) {
+  r_df$Sales[i] <- as.numeric(str_remove_all(str_remove_all(r_df$Total.Taxable[i], "\\$"), ","))
+  r_df$Num_Units[i] <- as.numeric(str_remove_all(r_df$Units[i], ","))
+}
+
 #Removes some columns that aren't used in our analysis
-r_df <- select(r_df, year_num, quarter_num, City, County, Total.Taxable, Units)
+r_df <- select(r_df, Year, year_num, quarter_num, City, County, Sales, Num_Units)
 
 v_df <- group_by(v_df, Year, Quarter, Recip_County)
 v_df <- summarise_all(v_df, max)
@@ -78,3 +84,29 @@ v_df <- summarise_all(v_df, max)
 unified_df <- merge(x = r_df, y = v_df, by.x = c("year_num", "quarter_num", "County"), by.y = c("Year", "Quarter", "Recip_County"))
 
 unified_df <- arrange(unified_df, City, year_num, quarter_num)
+
+#Added new numeric column
+for (i in 1:nrow(unified_df)) {
+  county = unified_df$County[i]
+  year = unified_df$year_num[i]
+  quarter = unified_df$quarter_num[i]
+  unified_df$county_avg_quarterly_sale_per_capita[i] <-  sum(unified_df[unified_df$County == county & unified_df$year_num == year & unified_df$quarter_num == quarter,'Sales']) / unified_df$Census2019[i]
+}
+
+#Added categorial column
+for (i in 1:nrow(unified_df)) {
+  if (unified_df$Sales[i] > 50000000) {
+    unified_df$Is_Commerce_Center[i] <- TRUE
+  } else {
+    unified_df$Is_Commerce_Center[i] <- FALSE
+  }
+}
+
+#Final clean
+na.omit(unified_df)
+
+#Summarization table
+counties <- group_by(unified_df, County, year_num, quarter_num, Census2019)
+counties <- summarize(counties, sum(Sales), sum(Num_Units), mean(Completeness_pct), mean(Administered_Dose1_Pop_Pct), mean(Series_Complete_Pop_Pct), mean(Booster_Doses_Vax_Pct))
+
+write.csv(unified_df, "unified.csv", row.names = FALSE)
